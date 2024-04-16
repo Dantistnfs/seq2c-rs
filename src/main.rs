e15024fa8ad741661ad74a6581b659d93e259fa4
Stanislav Zubenko <5831286+Dantistnfs@users.noreply.github.com>
2024-04-15 23:25:47 -0300

    feat(src/main.rs): use of array interval tree instead of default one provided by rust-bio

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use bio::io::bed;
 use bio::data_structures::interval_tree::*;
 use bio::bio_types::genome::AbstractInterval;
 
+use std::cell::RefCell;
 
 
 #[derive(Parser)]
@@ -35,7 +36,7 @@ struct Cli {
 #[derive(Debug, Clone)]
 struct RegionWithName {
     name: String,
-    count: i64,
+    count: RefCell<i64>,
 }
 
 #[derive(Debug, PartialOrd, Ord, PartialEq, Eq)]
@@ -67,7 +68,7 @@ fn main(){
             cli.threads
     };
 
-    let mut bed_map: HashMap<String,IntervalTree<i64, RegionWithName>> = HashMap::new();
+    let mut bed_map: HashMap<String,ArrayBackedIntervalTree<i64, RegionWithName>> = HashMap::new();
 
     let mut bed_chrom_order = Vec::new();
     let mut reader = File::open(cli.bed).map(BufReader::new).map(bed::Reader::new).unwrap();
@@ -75,12 +76,17 @@ fn main(){
         let rec = record.expect("Error reading record.");
         //println!("{rec:?}");
         let bed_map_chrom = bed_map.entry(rec.chrom().to_string()).or_default();
-        bed_map_chrom.insert(rec.start() as i64..rec.end() as i64,RegionWithName{ name:rec.name().expect("BED record does not define name").to_string(), count: 0});
+        bed_map_chrom.insert(rec.start() as i64..rec.end() as i64,RegionWithName{ name:rec.name().expect("BED record does not define name").to_string(), count: RefCell::new(0)});
         bed_chrom_order.push(rec.chrom().to_string());
     }
 
     // Cleanup chrom ordering from duplicates
     bed_chrom_order.dedup();
+
+    // Trees needs indexing before find method
+    for mut interval_tree in bed_map.values_mut() {
+        interval_tree.index();
+    }
 
 
     let mut bam = bam::Reader::from_path(cli.bam).unwrap();
@@ -110,12 +116,14 @@ fn main(){
             Some(bed_chrom) => bed_chrom,
             _ => continue,
         };
-        for mut interval in bed_map_chrom.find_mut(start-1..end+1) {
+        for interval in bed_map_chrom.find(start-1..end+1) {
             let (interval_start, interval_end) = (interval.interval().start, interval.interval().end);
 
-            let interval_data = interval.data(); //Mutable reference here
+            let interval_data = interval.data(); 
             if interval_data.name != "." { //Skip calculation of coverage for unnamed regions
-                interval_data.count += calculate_coverage(start..end, interval_start..interval_end);
+                let mut count = interval_data.count.borrow_mut(); //Mutable borrow, but happens
+                //only in one place, so it's fine
+                *count += calculate_coverage(start..end, interval_start..interval_end);
             }
 
         }
@@ -125,11 +133,11 @@ fn main(){
     println!("Sample\tGene\tChr\tStart\tEnd\tTag\tLength\tMeanDepth");
     for chrom in bed_chrom_order {
         let chrom_tree = bed_map.get_mut(&chrom).unwrap(); //Safe to unwrap since it's guaranteed that we will have a hit
-        let mut output = chrom_tree.find(0..i64::MAX)
+        let mut output = chrom_tree.find(0..i64::MAX).iter()
                                 .map(|e| OutputRegion{start:e.interval().start,
                                                         end:e.interval().end,
                                                     name:e.data().name.clone(),
-                                                    count:e.data().count})
+                                                    count:e.data().count.borrow().clone()})
                                 .collect::<Vec<_>>();
 
         output.sort();
