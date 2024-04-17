8c45b94868abc24648e5f98bd8c9bc9c5f9d5395
Stanislav Zubenko <5831286+Dantistnfs@users.noreply.github.com>
2024-04-17 18:52:32 -0300

    refactor(main): optimize code and improve performance, decrease time complexity

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,21 @@ use rust_htslib::{bam, bam::Read, bam::record::Cigar};
 use rust_htslib::bam::ext::BamRecordExtensions;
 
 use bio::io::bed;
-use bio::data_structures::interval_tree::*;
+//use bio::data_structures::interval_tree::*;
 use bio::bio_types::genome::AbstractInterval;
 
 use std::cell::RefCell;
+
+use coitrees::*;
+use rustc_hash::FxHashMap;
+use std::collections::BTreeMap;
+use indexmap::IndexMap;
+use fnv::FnvBuildHasher;
+use fxhash::FxBuildHasher;
+
+
+type FnvIndexMap<K, V> = IndexMap<K, V, FnvBuildHasher>;
+type FxIndexMap<K, V> = IndexMap<K, V, FxBuildHasher>;
 
 
 #[derive(Parser)]
@@ -57,47 +68,76 @@ fn calculate_coverage(a: std::ops::Range<i64>, b: std::ops::Range<i64>) -> i64 {
 }
 
 
+
+fn update_node(start: i64, end: i64, interval: &IntervalNode<RegionWithName, u32>) {
+    let metadata = &interval.metadata;
+    if metadata.name != "." { //Skip calculation of coverage for unnamed regions
+        let mut count = metadata.count.borrow_mut(); //Mutable borrow, but happens only in one thread, so it's fine
+        *count += calculate_coverage(start..end, interval.first as i64..interval.last as i64);
+    }
+}
+
+
+
 fn main(){
     let cli = Cli::parse();
     let sample_name = cli.sample_name;
     let mimic_perl_output = cli.mimic_perl_output;
+    eprintln!("Started");
 
     let bam_threads = if cli.threads == 0 {
             available_parallelism().expect("Wasn't able to automatically reconize number of threads, please set it by setting --threads argument manually").get()
         } else {
             cli.threads
     };
+    eprintln!("Using {bam_threads} threads for reading bam file");
 
-    let mut bed_map: HashMap<String,ArrayBackedIntervalTree<i64, RegionWithName>> = HashMap::new();
+    let mut nodes: FxHashMap<String, Vec<Interval<RegionWithName>>> = FxHashMap::default();
+    let mut bed_map: FxHashMap<String, COITree<RegionWithName, u32>> = FxHashMap::default();
 
+    eprintln!("Reading bed file");
     let mut bed_chrom_order = Vec::new();
     let mut reader = File::open(cli.bed).map(BufReader::new).map(bed::Reader::new).unwrap();
     for record in reader.records() {
         let rec = record.expect("Error reading record.");
-        //println!("{rec:?}");
-        let bed_map_chrom = bed_map.entry(rec.chrom().to_string()).or_default();
-        bed_map_chrom.insert(rec.start() as i64..rec.end() as i64,RegionWithName{ name:rec.name().expect("BED record does not define name").to_string(), count: RefCell::new(0)});
+        let node_vec = nodes.entry(rec.chrom().to_string()).or_insert(Vec::new());
+        node_vec.push(
+                        Interval::new(rec.start() as i32, 
+                                        rec.end() as i32,
+                                        RegionWithName{ 
+                                            name:rec.name().expect("BED record does not define name").to_string(), 
+                                            count: RefCell::new(0)
+                                        }
+                                    )
+                        );
         bed_chrom_order.push(rec.chrom().to_string());
     }
+
+    for (chrom, chrom_nodes) in nodes {
+        bed_map.insert(chrom, COITree::new(&chrom_nodes));
+    }
+    eprintln!("Reading bed file finished");
 
     // Cleanup chrom ordering from duplicates
     bed_chrom_order.dedup();
 
-    // Trees needs indexing before find method
-    for mut interval_tree in bed_map.values_mut() {
-        interval_tree.index();
+    // Convert COITree to Querent that stores info about last region to optinize serach
+    let mut querents = FnvIndexMap::<String, COITreeSortedQuerent<RegionWithName, u32>>::default();
+    for (seqname, tree) in &bed_map {
+        querents.insert(seqname.clone(), COITreeSortedQuerent::new(tree));
     }
 
+    eprintln!("Starting processing bam file");
 
     let mut bam = bam::Reader::from_path(cli.bam).unwrap();
     let _ = bam.set_threads(bam_threads).expect("Error in setting number of threads for loading bam file");
 
-    for r in bam.records() {
-        let record = r.unwrap();
-        if record.tid() < 0 {
+    for r in bam.rc_records() {
+        let record = r.expect("Failure parsing Bam file");
+        if record.is_supplementary() { //skip supplementary aligments
             continue;
         }
-        if record.is_supplementary() { //skip supplementary aligments
+        if record.tid() < 0 {
             continue;
         }
         let start = record.reference_start() + 1;  //becuase start position will be included
@@ -112,33 +152,30 @@ fn main(){
                 })
                 .sum::<u32>() as i64;
 
-        let bed_map_chrom = match bed_map.get_mut(chrom) {
-            Some(bed_chrom) => bed_chrom,
+        let querent_chrom = match querents.get_mut(chrom) {
+            Some(querent_chrom) => querent_chrom,
             _ => continue,
         };
-        for interval in bed_map_chrom.find(start-1..end+1) {
-            let (interval_start, interval_end) = (interval.interval().start, interval.interval().end);
-
-            let interval_data = interval.data(); 
-            if interval_data.name != "." { //Skip calculation of coverage for unnamed regions
-                let mut count = interval_data.count.borrow_mut(); //Mutable borrow, but happens
-                //only in one place, so it's fine
-                *count += calculate_coverage(start..end, interval_start..interval_end);
-            }
-
-        }
+        querent_chrom.query((start-1) as i32, (end+1) as i32, |node| {update_node(start, end, node)}); // Runs update_node on
+        // each interval in tree that has intersection with query interval
     }
 
-    // Write the header
-    println!("Sample\tGene\tChr\tStart\tEnd\tTag\tLength\tMeanDepth");
+    eprintln!("Finished processing bam file");
+
+    eprintln!("Outputing result into stdout");
+
+    // Prepare the header
+    let mut output_string = String::from("Sample\tGene\tChr\tStart\tEnd\tTag\tLength\tMeanDepth\n");
+
     for chrom in bed_chrom_order {
-        let chrom_tree = bed_map.get_mut(&chrom).unwrap(); //Safe to unwrap since it's guaranteed that we will have a hit
-        let mut output = chrom_tree.find(0..i64::MAX).iter()
-                                .map(|e| OutputRegion{start:e.interval().start,
-                                                        end:e.interval().end,
-                                                    name:e.data().name.clone(),
-                                                    count:e.data().count.borrow().clone()})
-                                .collect::<Vec<_>>();
+        let chrom_tree = querents.get_mut(&chrom).unwrap(); //Safe to unwrap since it's guaranteed that we will have a hit
+        //let mut output = chrom_tree.iter()
+        let mut output = Vec::new();
+        chrom_tree.query(0, i32::MAX, |node| {output.push(OutputRegion{start:node.first as i64,
+                                                        end:node.last as i64,
+                                                        name:node.metadata.name.clone(),
+                                                        count:node.metadata.count.borrow().clone()})
+                                            });
 
         output.sort();
 
@@ -153,7 +190,7 @@ fn main(){
                 if !current_gene.is_empty() {
                     // Calculate and write aggregated data for the previous gene
                     let mean_depth = if total_length > 0 { total_count as f64 / total_length as f64} else { 0.0 };
-                    println!("{sample_name}\t{current_gene}\t{chrom}\t{current_start}\t{current_end}\tWhole-Gene\t{total_length}\t{mean_depth:.2}");
+                    output_string += format!("{sample_name}\t{current_gene}\t{chrom}\t{current_start}\t{current_end}\tWhole-Gene\t{total_length}\t{mean_depth:.2}").as_str();
                 }
                 // Reset
                 current_gene = &region.name;
@@ -174,7 +211,7 @@ fn main(){
             if region.end > current_end {
                 current_end = region.end;
             }
-            println!("{sample_name}\t{}\t{chrom}\t{}\t{}\tAmplicon\t{}\t{:.2}", region.name, region.start, region.end, length, count as f64 /length as f64);
+            output_string += format!("{sample_name}\t{}\t{chrom}\t{}\t{}\tAmplicon\t{}\t{:.2}", region.name, region.start, region.end, length, count as f64 /length as f64).as_str();
             total_length += length;
             total_count += count;
 
@@ -182,7 +219,9 @@ fn main(){
 
         // at the end of the vector, write aggregated line for the last gene
         let mean_depth = if total_length > 0 { total_count as f64 / total_length as f64 } else { 0.0 };
-        println!("{sample_name}\t{current_gene}\t{chrom}\t{current_start}\t{current_end}\tWhole-Gene\t{total_length}\t{mean_depth:.2}");
-
+        output_string += format!("{sample_name}\t{current_gene}\t{chrom}\t{current_start}\t{current_end}\tWhole-Gene\t{total_length}\t{mean_depth:.2}").as_str();
     }
+    print!("{}", output_string);
+
+    eprintln!("Done");
 }
